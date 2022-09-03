29390ca785a7905274d53596b739e53546065ebd
ma631 <73124430+ma631@users.noreply.github.com>
2022-09-03 21:54:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,75 +3,94 @@ let weatherapi={
     baseurl: "https://api.openweathermap.org/data/2.5/weather"
 }
 
-const search=document.getElementById("search-bar");
 
-search.addEventListener('keypress',(event) =>{
-    if(event.keyCode == 13) {
-        console.log(search.value);
-        getweather(search.value)
+const wrapper = document.querySelector(".wrapper"),
+inputPart = document.querySelector(".input-part"),
+infoTxt = inputPart.querySelector(".info-txt"),
+inputField = inputPart.querySelector("input"),
+locationBtn = inputPart.querySelector("button"),
+weatherPart = wrapper.querySelector(".weather-part"),
+wIcon = weatherPart.querySelector("img"),
+arrowBack = wrapper.querySelector("header i");
+
+let api;
+
+inputField.addEventListener("keyup", e =>{
+    if(e.key == "Enter" && inputField.value != ""){
+        requestApi(inputField.value);
     }
-   
 });
 
-function getweather(city){
-    fetch(`${weatherapi.baseurl}?q= ${city}&appid=${weatherapi.apikey}&units=metric`)
-    .then(weather => {
-        return weather.json();
-    }).then(showweather);
+locationBtn.addEventListener("click", () =>{
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(onSuccess);
+    }else{
+        alert("Your browser not support geolocation api");
+    }
+});
 
+function requestApi(city){
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weatherapi.apikey}`;
+    fetchData();
 }
 
-function showweather(weather){
-    console.log(weather);
-
-    let city=document.getElementById("city");
-    city.innerText=`${weather.name}, ${weather.sys.country}`;
-
-    let temp=document.getElementById("temp");
-    temp.innerHTML=`${Math.round(weather.main.temp)}&deg;C`;
-
-    let mintemp=document.getElementById("min-max");
-    mintemp.innerHTML=`${Math.floor(weather.main.temp_min)}&deg;C (min)/ ${Math.ceil(weather.main.temp_max)}&deg;C (max) `;
-
-    let weathertype=document.getElementById("weather");
-    weathertype.innerText=`${weather.weather[0].main}`;
-
-    if(weathertype.textContent=='Clear'){
-        document.body.style.backgroundImage="url('clear.jpg')";
-    }
-    else if(weathertype.textContent=='Rain'){
-        document.body.style.backgroundImage="url('rain.jpg')";
-    }
-    else if(weathertype.textContent=='Clouds'){
-        document.body.style.backgroundImage="url('clouds.jpg')";
-    }
-    else if(weathertype.textContent=='Haze'){
-        document.body.style.backgroundImage="url('haze.jpg')";
-    }
-    else if(weathertype.textContent=='Snow'){
-        document.body.style.backgroundImage="url('snow.jpg')";
-    }
-    else if(weathertype.textContent=='Thunderstorm'){
-        document.body.style.backgroundImage="url('thunderstorm.jpg')";
-    }
-
-
-    let date=document.getElementById("date");
-    let todaydate=new Date();
-    date.innerText=showdate(todaydate);
+function onSuccess(position){
+    const {latitude, longitude} = position.coords;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${weatherapi.apikey}`;
+    fetchData();
 }
 
-
-function showdate(date1){
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-    let year=date1.getFullYear();
-    let month=months[date1.getMonth()];
-    let date=date1.getDate();
-    let day=days[date1.getDay()];
-
-    return `${date} ${month} (${day}), ${year}`;
+function onError(error){
+    infoTxt.innerText = error.message;
+    infoTxt.classList.add("error");
 }
-   
+
+function fetchData(){
+    infoTxt.innerText = "Getting weather details...";
+    infoTxt.classList.add("pending");
+    fetch(api).then(res => res.json()).then(result => weatherDetails(result)).catch(() =>{
+        infoTxt.innerText = "Something went wrong";
+        infoTxt.classList.replace("pending", "error");
+    });
+}
+
+function weatherDetails(info){
+    if(info.cod == "404"){
+        infoTxt.classList.replace("pending", "error");
+        infoTxt.innerText = `${inputField.value} isn't a valid city name`;
+    }else{
+        const city = info.name;
+        const country = info.sys.country;
+        const {description, id} = info.weather[0];
+        const {temp, feels_like, humidity} = info.main;
+
+        if(id == 800){
+            wIcon.src = "clear.jpg";
+        }else if(id >= 200 && id <= 232){
+            wIcon.src = "thunderstorm.jpg";  
+        }else if(id >= 600 && id <= 622){
+            wIcon.src = "snow.jpg";
+        }else if(id >= 701 && id <= 781){
+            wIcon.src = "haze.jpg";
+        }else if(id >= 801 && id <= 804){
+            wIcon.src = "clouds.jpg";
+        }else if((id >= 500 && id <= 531) || (id >= 300 && id <= 321)){
+            wIcon.src = "rain.jpg";
+        }
+        
+        weatherPart.querySelector(".temp .numb").innerText = Math.floor(temp);
+        weatherPart.querySelector(".weather").innerText = description;
+        weatherPart.querySelector(".location span").innerText = `${city}, ${country}`;
+        weatherPart.querySelector(".temp .numb-2").innerText = Math.floor(feels_like);
+        weatherPart.querySelector(".humidity span").innerText = `${humidity}%`;
+        infoTxt.classList.remove("pending", "error");
+        infoTxt.innerText = "";
+        inputField.value = "";
+        wrapper.classList.add("active");
+    }
+}
+
+arrowBack.addEventListener("click", ()=>{
+    wrapper.classList.remove("active");
+});
+
